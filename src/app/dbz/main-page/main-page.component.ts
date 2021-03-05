eb1810753b68626694d50fd968ac1067c9a67a3b
Sebastian Ramirez <sramirez@conexionurl.com.co>
2021-03-05 10:02:01 -0500

    main-page

---
+++ src/app/dbz/main-page/main-page.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+interface Personaje {
+  nombre: string;
+  poder: number;
+}
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html'
+})
+export class MainPageComponent {
+
+  personajes: Personaje[] = [
+  {
+    nombre: 'Goku',
+    poder: 15000
+  },
+  {
+    nombre: 'Vegeta',
+    poder:	7500
+  }
+  ];
+
+  nuevo: Personaje = {
+    nombre: '',
+    poder: 0
+  };
+
+  // tslint:disable-next-line: typedef
+  agregar(){
+    if (this.nuevo.nombre.trim().length === 0){
+      return;
+    }
+    console.log(this.nuevo);
+
+    this.personajes.push(this.nuevo);
+
+    this.nuevo = {
+      nombre: '',
+      poder: 0
+    };
+  }
+}
